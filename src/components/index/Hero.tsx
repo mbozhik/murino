699ddf0cb63e0118046d7b894b6ce36be464ddec
bozzhik <bozzhik@ya.ru>
2024-01-29 20:58:44 +0300

    add `HeroSocials`

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -5,6 +5,7 @@ import Button from '../ui/Button'
 
 import HeroBackground from '../../assets/index/hero-background.png'
 import HeroText from '../../assets/index/hero-text.svg'
+import HeroSocials from './HeroSocials'
 
 const screenHeight = '!sm:h-[100svh] h-[100vh]'
 
@@ -15,10 +16,11 @@ export default function Hero() {
       <div className="grid h-[75vh] place-items-center">
         <Image className="object-cover" src={HeroText} alt="" />
       </div>
-      <div className="mx-20 mt-5">
+      <div className="flex items-center justify-between mx-20">
         <Button style="hero" link="#booking">
           Забронировать
         </Button>
+        <HeroSocials />
       </div>
 
       <div className="absolute inset-0 bg-black s-full -z-10">
---
+++ src/components/index/HeroSocials.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+import TelImage from '../../assets/index/socials/tel.svg'
+import VkImage from '../../assets/index/socials/vk.svg'
+import TgImage from '../../assets/index/socials/tg.svg'
+
+const icons = [
+  {link: 'tel:+79217518703', target: '', image: TelImage},
+  {link: 'https://vk.com/murino_arena', target: '_blank', image: VkImage},
+  {link: 'https://t.me/murino_arena', target: '_blank', image: TgImage},
+]
+
+export default function HeroSocials() {
+  return (
+    <div className="flex gap-4">
+      {icons.map((icon, index) => (
+        <a href={icon.link} target={icon.target} title="icon" key={index} className="duration-200 s-14 hover:scale-110">
+          <Image className="object-cover s-full" src={icon.image} alt={`${icon.link} icon`} width={48} height={48} />
+        </a>
+      ))}
+    </div>
+  )
+}
