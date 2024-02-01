9fb4f667f117378d949e56f728b2e510e710d263
bozzhik <bozzhik@ya.ru>
2024-02-02 01:25:11 +0300

    destroy `HeroSocials` component

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -1,11 +1,33 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Header from '../ui/Header/Header'
 import Button from '../ui/Button'
 
 import HeroBackground from '../../assets/index/hero-background.png'
 import HeroText from '../../assets/index/hero-text.svg'
-import HeroSocials from './HeroSocials'
+
+import TelImage from '../../assets/index/socials/tel.svg'
+import VkImage from '../../assets/index/socials/vk.svg'
+import TgImage from '../../assets/index/socials/tg.svg'
+
+const icons = [
+  {link: 'tel:+79217518703', target: '', image: TelImage},
+  {link: 'https://vk.com/murino_arena', target: '_blank', image: VkImage},
+  {link: 'https://t.me/murino_arena', target: '_blank', image: TgImage},
+]
+
+function HeroSocials() {
+  return (
+    <div className="flex gap-4">
+      {icons.map((icon, index) => (
+        <Link href={icon.link} target={icon.target} title="icon" key={index} className="duration-200 s-14 xl:s-10 hover:scale-110">
+          <Image className="object-cover s-full" src={icon.image} alt={`${icon.link} icon`} width={48} height={48} />
+        </Link>
+      ))}
+    </div>
+  )
+}
 
 const screenHeight = 'h-[100vh]'
 
