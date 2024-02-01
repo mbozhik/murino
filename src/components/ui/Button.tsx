2835c82b7c5425e1230e3e12cb0b77967e4010c7
bozzhik <bozzhik@ya.ru>
2024-02-01 15:52:29 +0300

    upgrade `Button` customizable component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 interface ButtonProps {
-  style?: 'hero' | 'heading' | 'simple'
+  style: 'hero' | 'heading' | 'simple'
   link?: string
   classes?: string
   children: React.ReactNode
@@ -9,14 +9,23 @@ interface ButtonProps {
 
 export default function Button({link, classes, children, style = 'simple'}: ButtonProps) {
   const buttonStyles = {
+    default: 'text-center px-16 py-6 xl:px-10 xl:py-4 sm:py-2 text-3xl xl:text-xl sm:text-lg uppercase font-medium tracking-tighter duration-200',
     hero: 'bg-white text-custom-gray rounded-full hover:bg-custom-e4',
-    heading: 'bg-custom-green text-white',
+    heading: 'bg-custom-green text-white rounded-[15px]',
     simple: 'bg-custom-gray',
   }
 
+  const classNames = [buttonStyles.default, buttonStyles[style], classes].filter(Boolean).join(' ')
+
   return (
-    <Link href={link} className={`text-center px-16 py-6 xl:px-10 xl:py-4 sm:py-2 text-3xl xl:text-xl sm:text-lg uppercase font-medium tracking-tighter duration-200 ${buttonStyles[style]} ${classes}`}>
-      {children}
-    </Link>
+    <>
+      {link ? (
+        <Link href={link} className={classNames}>
+          {children}
+        </Link>
+      ) : (
+        <div className={classNames}>{children}</div>
+      )}
+    </>
   )
 }
