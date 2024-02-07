1a679af8989c66d4eba3e755ffb575d25ff6f301
bozzhik <bozzhik@ya.ru>
2024-02-07 18:35:38 +0300

    add `custom` addres in `Footer` & refactor `Footer`

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 
 export default function Footer() {
+  // Array of link information
+  const links = [
+    {text: 'Мурино, Заводской проезд 4', href: 'https://yandex.ru/maps/org/murino_arena/76918350984/?from=mapframe&ll=30.474895%2C60.043823&source=mapframe&utm_source=mapframe&z=16'},
+    {text: '+7 (921) 751-87-03', href: 'tel:+79217518703'},
+  ]
+
   return (
     <div className="flex justify-between mx-10 mb-5 text-lg sm:mb-24 sm:gap-3 sm:mx-3 sm:flex-col xl:text-base sm:text-xs text-custom-gray">
       <div className="flex items-center gap-5 sm:gap-3 sm:justify-center">
@@ -9,11 +15,11 @@ export default function Footer() {
       </div>
 
       <div className="flex gap-10 p-3 sm:grid sm:grid-cols-2 sm:gap-2 sm:border-2 sm:border-custom-e4 rounded-md">
-        <h1>Юр. адрес: ул. Ленина д.1</h1>
-        <h1>Факт. адрес: ул. Сталина д.2</h1>
-        <Link href="tel:+79217518703" className="underline duration-200 hover:text-custom-95">
-          +7 (921) 751-87-03
-        </Link>
+        {links.map((link, index) => (
+          <Link key={index} href={link.href} target="_blank" className="underline duration-200 hover:no-underline">
+            {link.text}
+          </Link>
+        ))}
         <h1>Круглосуточно</h1>
       </div>
     </div>
