9bbde0c14e1b66b2563d7ecb89d1c2a0db56daa1
bozzhik <bozzhik@ya.ru>
2024-02-12 02:41:49 +0300

    fix issue with positioning child elements of `Hero` block

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -35,13 +35,13 @@ const screenHeight = 'h-[100vh]'
 
 export default function Hero() {
   return (
-    <section id="hero" className={`relative pt-10 xs:pb-10 sm:pt-5 duration-500 ${screenHeight}`}>
+    <section id="hero" className={`relative flex flex-col flex-1 justify-between py-12 sm:py-7 duration-500 ${screenHeight}`}>
       <Header />
-      <div className="grid h-[75vh] xl:h-[74vh] sm:h-[70vh] xs:h-[62.5vh] place-items-center">
+      <div className="grid place-items-center">
         <Image className="xl:w-[30%] sm:w-[70%] object-cover" src={HeroText} alt="" />
       </div>
       <div className="flex items-center justify-between mx-20 sm:flex-col sm:gap-5 sm:mx-5">
-        <Button style="hero" link="#booking" classes="sm:text-lg sm:w-full">
+        <Button style="hero" link="#prices" classes="sm:text-lg sm:w-full">
           Забронировать
         </Button>
         <HeroSocials />
