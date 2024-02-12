bf059fe80f370297608aad490426a1dddfc268b1
bozzhik <bozzhik@ya.ru>
2024-02-12 03:01:52 +0300

    clean project after installation `shadcn-ui`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,12 @@
 const plugin = require('tailwindcss/plugin')
+
 import type {Config} from 'tailwindcss'
 
-const config = {
+const config: Config = {
   mode: 'jit',
-  darkMode: ['class'],
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './components/**/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}', './src/components/index/Hero.tsx'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       fontFamily: {
         sans: ['var(--font-opensans)', 'ui-sans-serif', 'system-ui', '-apple-system', 'Roboto', 'sans-serif'],
@@ -40,20 +33,6 @@ const config = {
         base: '0px 0px 10px rgba(0, 0, 0, 0.25)',
         card: '0px 4px 20px rgba(0, 0, 0, 0.25)',
       },
-      keyframes: {
-        'accordion-down': {
-          from: {height: '0'},
-          to: {height: 'var(--radix-accordion-content-height)'},
-        },
-        'accordion-up': {
-          from: {height: 'var(--radix-accordion-content-height)'},
-          to: {height: '0'},
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
     },
     screens: {
       xl: {max: '1536px'},
@@ -68,6 +47,6 @@ const config = {
       api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
     }),
   ],
-} satisfies Config
+}
 
 export default config
