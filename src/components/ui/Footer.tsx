7afdf8239c6fb7236dc5f51c8b0d297521d19c3c
bozzhik <bozzhik@ya.ru>
2024-02-16 02:28:37 +0300

    optimize `Footer` for mobile

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
   const {copy, tel} = linksData
 
   return (
-    <div className="flex justify-between mx-10 mb-5 text-lg sm:mb-24 sm:gap-3 sm:mx-3 sm:flex-col xl:text-base sm:text-xs text-custom-gray">
-      <div className="flex items-center gap-5 sm:gap-3 sm:justify-center">
+    <div className="flex justify-between p-3 mx-10 mb-5 text-lg sm:flex-col sm:mb-24 sm:gap-2 sm:mx-3 xl:text-base sm:text-xs sm:tracking-tight text-custom-gray sm:border-2 sm:border-custom-e4">
+      <div className="flex items-center gap-5 sm:justify-center sm:hidden">
         <h1 className="text-xl">©</h1>
         <h1>OOO «Кронос»</h1>
       </div>
 
-      <div className="flex gap-10 p-3 sm:grid sm:grid-cols-2 sm:gap-2 sm:border-2 sm:border-custom-e4 rounded-md">
+      <div className="flex gap-10 rounded-md sm:flex-wrap sm:justify-center sm:gap-x-5 sm:gap-y-2">
         {copy && (
           <Link href={copy.href} target="_blank" className="underline duration-200 hover:no-underline">
             {copy.text}
@@ -24,6 +24,9 @@ export default function Footer() {
             {tel.text}
           </Link>
         )}
+
+        <h1 className="hidden sm:inline">OOO «Кронос»</h1>
+
         <h1>Круглосуточно</h1>
       </div>
     </div>
