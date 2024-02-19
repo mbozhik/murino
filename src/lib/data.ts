dbeb2e4a1e492fb958777d8e7c2326312c193283
bozzhik <bozzhik@ya.ru>
2024-02-19 05:27:50 +0300

    add new page `/metro`

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,10 +1,9 @@
 const socialName = 'MURINO_ARENA'
-const mapsLink = 'https://yandex.ru/maps/-/CDBYJM37'
 
 export const linksData = {
   tel: {text: '+7 (921) 751-87-03', href: 'tel:+79217518703'},
   vk: {text: socialName, href: 'https://vk.com/murino_arena'},
   tg: {text: socialName, href: 'https://t.me/murino_arena'},
-  copy: {text: 'Мурино, Заводской проезд 4', href: mapsLink},
-  map: {text: 'Как добраться на метро', href: mapsLink},
+  copy: {text: 'Мурино, Заводской проезд 4', href: 'https://yandex.ru/maps/-/CDBYJM37'},
+  map: {text: 'Как добраться на метро', href: '/metro'},
 }
